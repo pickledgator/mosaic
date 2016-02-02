50aedc08aceac4cd9acb52a77f41df9bd0db5593
Nic <nic@Nics-MacBook.local>
2016-02-02 11:42:43 -0800

    stuff

--- mosaic.py
+++ mosaic.py
@@ -8,6 +8,7 @@ import numpy as np
 from numpy.linalg import inv
 import imutils
 import cv2
+import csv
 
 logging.basicConfig(format='%(asctime)s %(levelname)s [%(module)s] %(message)s', level=logging.INFO)
 
@@ -21,17 +22,31 @@ class Stitch:
 			self.logger.error("Directory does not exist!")
 			return
 		self.dirPath = dirPath
+
 		# grab filenames from directory
 		self.filenames = self.getFilenames(self.dirPath)
 		if self.filenames == None:
 			self.logger.error("Error reading filenames, was directory empty?")
 			return
+
 		# create cvimage objects
 		self.images = []
 		for i in self.filenames:
 			self.images.append(cv2.imread(i))
 			self.images[-1] = imutils.resize(self.images[-1], width=400)
 			self.images[-1] = cv2.copyMakeBorder(self.images[-1],20,20,20,20,cv2.BORDER_CONSTANT,0)
+			rot = cv2.getRotationMatrix2D((self.images[-1].shape[1],self.images[-1].shape[0]),10,1)
+			dst = cv2.warpAffine(self.images[-1],rot,(self.images[-1].shape[1],self.images[-1].shape[0]))
+			cv2.imshow("Ori",self.images[-1])
+			cv2.imshow("Rot",dst)
+			cv2.waitKey(0)
+
+		# load pose data
+		self.logger.info("Loading pose.csv...")
+		reader = csv.DictReader(open(self.dirPath+'/pose.csv'))
+		poseData = []
+		for row in reader:
+			poseData.append(row)
 
 		# init other variable lists
 		self.kps = []
@@ -57,28 +72,13 @@ class Stitch:
 
 	def process(self, ratio=0.75, reprojThresh=4.0, showMatches=False):
 
-
-		# imageA = cv2.imread(self.images[0])
-		# imageB = cv2.imread(self.images[1])
-		# imageA = imutils.resize(imageA, width=400)
-		# imageB = imutils.resize(imageB, width=400)
-		# imageA = self.images[0]
-		# imageB = self.images[1]
-		# imageC = self.images[2]
-
-		# unpack the images, then detect keypoints and extract
-		# local invariant descriptors from them
-		#(imageB, imageA) = images
-		# (kpsA, featuresA) = self.extractFeatures(imageA)
-		# (kpsB, featuresB) = self.extractFeatures(imageB)
-		# (kpsC, featuresC) = self.extractFeatures(imageC)
 		for img in self.images:
 			(keypts, feats) = self.extractFeatures(img)
 			self.kps.append(keypts)
 			self.features.append(feats)
 
 		for i,img in enumerate(self.images[:-1]):
-			kpsMatches = self.matchKeypoints(self.kps[i+1],self.kps[i], self.features[i+1], self.features[i], ratio, reprojThresh)
+			kpsMatches = self.matchKeypoints(self.kps[i+1], self.kps[i], self.features[i+1], self.features[i], ratio, reprojThresh)
 			if kpsMatches == None:
 				continue
 			self.M.append(kpsMatches)
@@ -92,29 +92,9 @@ class Stitch:
 				chainedH = chainedH.dot(h)
 			res = cv2.warpPerspective(self.images[i+1], chainedH, self.windowSize)
 			self.results.append(res)
-
-		# match features between the two images
-		# M = self.matchKeypoints(kpsB, kpsA, featuresB, featuresA, ratio, reprojThresh)
-		# M2 = self.matchKeypoints(kpsC, kpsB, featuresC, featuresB, ratio, reprojThresh)
-
-		# if the match is None, then there aren't enough matched
-		# keypoints to create a panorama
-		# if M is None:
-			# return False
-
-		# if M2 is None:
-		# 	return False
-
-		# otherwise, apply a perspective warp to stitch the images
-		# together
-		# (matches, H, status) = M
-		# (matches2, H2, status2) = M2
-		# shift = (0,0)
-		# Ht = np.array([[1,0,shift[0]],[0,1,shift[1]],[0,0,1]], np.float64)
-		# result = cv2.warpPerspective(imageB, Ht.dot(H), (1000,600))
-		#result[shift[1]:imageA.shape[0]+shift[1], shift[0]:imageA.shape[1]+shift[0]] = imageA
-		# result2 = cv2.warpPerspective(imageC, Ht.dot(H2.dot(H)), (1000,600))
-		#result2[shift[1]:imageA.shape[0]+shift[1], shift[0]:imageA.shape[1]+shift[0]] = imageA
+			vis = self.drawMatches(self.images[i+1], self.images[i], self.kps[i+1], self.kps[i], matches, status)
+			# cv2.imshow("Matches", vis)
+			# cv2.waitKey(0)
 
 		# create some empty images for use in combining results
 		base = np.zeros((self.windowSize[1],self.windowSize[0],3), np.uint8)
@@ -122,25 +102,18 @@ class Stitch:
 
 		# add base image
 		base[self.shift[1]:self.images[0].shape[0]+self.shift[1], self.shift[0]:self.images[0].shape[1]+self.shift[0]] = self.images[0]
-		container = self.addImage(base,container)
-		#container = cv2.addWeighted(container, 0.5, base, 0.5, 0.0)
+		container = self.addImage(base,container, transparent=False)
+
 		# add other images that have been warped
 		for res in self.results:
-			container = self.addImage(res,container)
-			# container = cv2.addWeighted(container, 0.5, res, 0.5, 0.0)
+			container = self.addImage(res, container, transparent=False)
 
 		# check to see if the keypoint matches should be visualized
-		if showMatches:
-			for i,image in enumerate(self.images):
-				cv2.imshow("Image {}".format(i), image)
-			# vis = self.drawMatches(imageB, imageA, kpsB, kpsA, matches,status)
-			# cv2.imshow("Keypoint Matches", vis)
-			cv2.imshow("Result", container)
-			cv2.waitKey(0)
-
-			# return a tuple of the stitched image and the
-			# visualization
-			# return (result, vis)
+		#if showMatches:
+			#for i,image in enumerate(self.images):
+			#	cv2.imshow("Image {}".format(i), image)
+			#cv2.imshow("Result", container)
+			#cv2.waitKey(0)
 
 		# return the stitched image
 		return True
@@ -153,36 +126,43 @@ class Stitch:
 				container.itemset((x,y,c),value)
 		return container
 
-	def addImage(self, image, container):
-		res = cv2.add(image, container)
+	def addImage(self, image, container, first=False, transparent=False):
+		if transparent:
+			con = cv2.addWeighted(container, 0.5, image, 0.5, 0.0)
+			cv2.imshow("Container", con)
+			cv2.waitKey(0)
+			return con
+
+		# if the container is empty, just return the full image
+		if first:
+			return image
+		# else threshold both images, find non-overlapping sections, add to container
+		greyImage = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+		greyContainer = cv2.cvtColor(container,cv2.COLOR_BGR2GRAY)
+		ret,threshImage = cv2.threshold(greyImage,10,255,cv2.THRESH_BINARY)
+		ret,threshContainer = cv2.threshold(greyContainer,10,255,cv2.THRESH_BINARY)
+		intersect = cv2.bitwise_and(threshImage, threshContainer)
+		mask = cv2.subtract(threshImage,intersect)
+		maskedImage = cv2.bitwise_and(image, image, mask=mask)
+		con = cv2.add(container, maskedImage)
+		cv2.imshow("Container", con)
+		cv2.waitKey(0)
+		#res = cv2.add(image, container)
 		#res = res / 2.0
-		return res
+		return con
 
 
 	def extractFeatures(self, image):
 		# convert the image to grayscale
 		gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-		# check to see if we are using OpenCV 3.X
-		# if self.isv3:
 		# detect and extract features from the image
 		self.logger.info("Running SIFT, extracting features...")
 		descriptor = cv2.xfeatures2d.SIFT_create()
 		(kps, features) = descriptor.detectAndCompute(image, None)
 		self.logger.info("Found {} keypoints in frame".format(len(kps)))
 
-		# # otherwise, we are using OpenCV 2.4.X
-		# else:
-		# 	# detect keypoints in the image
-		# 	detector = cv2.FeatureDetector_create("SIFT")
-		# 	kps = detector.detect(gray)
-
-		# 	# extract features from the image
-		# 	extractor = cv2.DescriptorExtractor_create("SIFT")
-		# 	(kps, features) = extractor.compute(gray, kps)
-
-		# convert the keypoints from KeyPoint objects to NumPy
-		# arrays
+		# convert the keypoints from KeyPoint objects to np
 		kps = np.float32([kp.pt for kp in kps])
 
 		# return a tuple of keypoints and features
@@ -192,6 +172,13 @@ class Stitch:
 		# compute the raw matches and initialize the list of actual
 		# matches
 		self.logger.info("Computing matches...")
+
+		# FLANN_INDEX_KDTREE = 0
+		# index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+		# search_params = dict(checks = 50)   # or pass empty dictionary
+		# flann = cv2.FlannBasedMatcher(index_params,search_params)
+		# rawMatches = flann.knnMatch(featuresA,featuresB,k=2)
+
 		matcher = cv2.DescriptorMatcher_create("BruteForce")
 		rawMatches = matcher.knnMatch(featuresA, featuresB, 2)
 
@@ -251,9 +238,9 @@ if __name__=="__main__":
 	args = vars(ap.parse_args())
 
 	mosaic = Stitch(args['dir'])
-	res = mosaic.process(showMatches=True)
-	if not res:
-		mosaic.logger.error('Mosaic failed.')
+	#res = mosaic.process(showMatches=True)
+	#if not res:
+	#	mosaic.logger.error('Mosaic failed.')
 
 
 
